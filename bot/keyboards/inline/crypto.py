0fe68e710d973f4d528edb314df64ac3a90e15f2
makakashan <vacetmax@gmail.com>
2025-11-05 13:40:38 +0100

    Feat: Combine crypto buttons and update handler

--- bot/keyboards/inline/crypto.py
+++ bot/keyboards/inline/crypto.py
@@ -4,10 +4,11 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 def crypto_keyboard() -> InlineKeyboardMarkup:
     """Create the crypto inline keyboard."""
     buttons = [
-        [InlineKeyboardButton(text="Bitcoin", callback_data="bitcoin")],
-        [InlineKeyboardButton(text="Ethereum", callback_data="ethereum")],
-        [InlineKeyboardButton(text="BNB", callback_data="bnb")],
-        [InlineKeyboardButton(text="SOL", callback_data="solana")],
+        [
+            InlineKeyboardButton(
+                text="Bitcoin and Etherium", callback_data="bitcoin and ethereum"
+            )
+        ],
         [InlineKeyboardButton(text="Back to main menu", callback_data="back_to_main")],
     ]
     keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
